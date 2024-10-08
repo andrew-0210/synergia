dba2330c13d440282e3a1cc7cf31a7c9601d21f6
Andrew Monthero <166970322+andrew-0210@users.noreply.github.com>
2024-10-08 20:09:29 +1100

    Update hero.tsx

--- src/ui/hero.tsx
+++ src/ui/hero.tsx
@@ -13,7 +13,7 @@ const Hero = () => {
   return (
     <div className="relative">
       <div className="absolute top-0 z-[-2] h-screen w-full bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]" />
-      <div className="absolute inset-0 z-[-1] h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,transparent_50%,#9a6aff_100%)]"></div>
+      <div className="absolute inset-0 z-[-1] h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,transparent_50%,#9a6aff_100%)]"/>
       <section className="container flex h-screen flex-col items-center justify-start gap-[1rem] py-[9rem]">
         <h1>
           A{" "}
