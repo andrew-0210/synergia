65f7c967fa297bf1e805b4b415cc4615aee9cf74
Andrew Monthero <andrewmonthero21@gmail.com>
2024-10-09 14:07:43 +1000

    form design changes

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,11 +5,11 @@ import React from "react";
 const Login = () => {
   return (
     <section className="flex h-screen flex-col items-center justify-start gap-[1rem] py-[9rem]">
-      <div>
+      <div className="">
         <h2>Login</h2>
         <p>Enter credentials to continue</p>
       </div>
-      <div>
+      <div className="flex flex-col gap-4 text-[#0e0e0e]">
         <input type="text" placeholder="Enter user id/email" />
         <input type="password" placeholder="Enter Password" />
         <Button intent="primary">Submit</Button>
