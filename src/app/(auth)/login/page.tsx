4c53c69c8e6ef183655b9148611ff10a34de147a
Andrew Monthero <andrewmonthero21@gmail.com>
2024-10-09 13:54:04 +1000

    test changes

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,6 +5,10 @@ const Login = () => {
   return (
     <section className="flex h-screen flex-col items-center justify-start gap-[1rem] py-[9rem]">
       <h2>Login</h2>
+      <div>
+        <h2>Login</h2>
+        <p>Enter credentials to continue</p>
+      </div>
       <Link href="/" className="text-[#b4e251]">
         Go back to Site
       </Link>
