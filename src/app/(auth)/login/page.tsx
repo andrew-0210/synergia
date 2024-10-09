791d448b2c1fa0e0a54b76c8a49fa333157f95a7
Andrew Monthero <andrewmonthero21@gmail.com>
2024-10-09 14:02:37 +1000

    small changes

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,9 +3,6 @@ import Link from "next/link";
 import React from "react";
 
 const Login = () => {
-  const handleForm = (e: { preventDefault: () => void }) => {
-    e.preventDefault();
-  };
   return (
     <section className="flex h-screen flex-col items-center justify-start gap-[1rem] py-[9rem]">
       <div>
@@ -13,11 +10,9 @@ const Login = () => {
         <p>Enter credentials to continue</p>
       </div>
       <div>
-        <form onSubmit={handleForm}>
-          <input type="text" placeholder="Enter user id/email" />
-          <input type="password" placeholder="Enter Password" />
-          <Button intent="primary">Submit</Button>
-        </form>
+        <input type="text" placeholder="Enter user id/email" />
+        <input type="password" placeholder="Enter Password" />
+        <Button intent="primary">Submit</Button>
       </div>
       <Link href="/" className="text-[#b4e251]">
         Go back to Site
