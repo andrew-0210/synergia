5a201b78e623cf5659d881417c47c222154e89c6
Andrew Monthero <166970322+andrew-0210@users.noreply.github.com>
2024-10-13 13:16:52 +0530

    Update layout.tsx

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { manrope, denton } from "../fonts/font";
 
 export const metadata: Metadata = {
-  title: "Synergia",
+  title: "Synergia-Login",
   description:
     "Synergia is your all-in-one HRM powerhouse! From seamless recruitment to effortless payroll and performance management, Synergia simplifies every aspect of HR for growing teams. Empower your business with a smarter, more efficient HR solution!",
 };
